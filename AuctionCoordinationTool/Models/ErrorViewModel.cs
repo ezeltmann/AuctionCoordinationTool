9726ed4a202c9af9cd8d1ff2372e1cf340127ab9
ezeltmann <eric.zeltmann@gmail.com>
2018-03-30 17:43:11 -0400

    Add project files.

---
+++ AuctionCoordinationTool/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuctionCoordinationTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
