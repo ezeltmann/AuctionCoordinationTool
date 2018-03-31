ed6b8f273b1a1addfc62d124d2456ef306705eeb
Eric Zeltmann <eric.zeltmann@gmail.com>
2018-03-31 00:15:16 -0400

    Removed unused code, added basic outlines for Bid, Bidder, and Paddle.

---
+++ AuctionCoordinationTool/Models/Bid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionCoordinationTool.Models
+{
+    public class Bid
+    {
+        public int BidId { get; set; }
+        public int PaddleId { get; set; }
+        public int DonationId { get; set; }
+        public int Units { get; set; }
+        public decimal CostPerUnit { get; set; }
+    }
+}
---
+++ AuctionCoordinationTool/Models/Paddle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionCoordinationTool.Models
+{
+    public class Paddle
+    {
+        public int PaddleId { get; set; }
+        public int BidderId { get; set; }
+    }
+}
